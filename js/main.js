002175522d7613230ad8d87ecf961b99e6dc799c
Lars <lars.bischhausen@gmail.com>
2023-11-15 16:25:38 +0100

    added template for impressum and datenschutz

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
   $("#logo").click(function () {
     $("#detailModal").modal("show");
   });
+  $("#policies").click(function () {
+    $("#furtherInformation").modal("show");
+  });
 });
